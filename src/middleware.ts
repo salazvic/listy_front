ac9ef7d00e3c8c12a191da9d3cd28c27eaf5bfd5
salazvic <sadelto3011@gmail.com>
2026-01-09 19:47:08 -0300

    error login 7

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,20 @@ import type { NextRequest } from 'next/server'
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('access_token')?.value
   const { pathname } = req.nextUrl
-
+console.log("entrando a middleware")
   const isAuthRoute = pathname.startsWith('/login') || pathname.startsWith('/register')
   const isProtectedRoute = pathname.startsWith('/lists')
 
   // NO logueado → ruta protegida
   if (!token && isProtectedRoute) {
     return NextResponse.redirect(new URL('/login', req.url))
+    console.log(" NO logueado → ruta protegida")
   }
 
   // Logueado → auth pages
   if (token && isAuthRoute) {
     return NextResponse.redirect(new URL('/lists', req.url))
+    console.log("Logueado → auth pages")
   }
 
   return NextResponse.next()
