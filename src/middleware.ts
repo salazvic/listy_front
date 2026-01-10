cc791d91778f4965fe878bc80e3fb08890bbecba
salazvic <sadelto3011@gmail.com>
2026-01-10 00:04:42 -0300

    error login 16

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,12 +7,13 @@ export function middleware(req: NextRequest) {
   const isAuthRoute = pathname.startsWith('/login') || pathname.startsWith('/register')
   const isProtectedRoute = pathname.startsWith('/lists')
 
+  console.log("Entrado a middleware")
   console.log("token middleware:", token)
 
   // NO logueado → ruta protegida
   if (!token && isProtectedRoute) {
-    return NextResponse.redirect(new URL('/login', req.url))
     console.log(" NO logueado → ruta protegida")
+    return NextResponse.redirect(new URL('/login', req.url))
   }
 
   // Logueado → auth pages
