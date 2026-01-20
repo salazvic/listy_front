308193da85b4cb653942f395040ef0e2b60ce0f7
salazvic <sadelto3011@gmail.com>
2026-01-20 13:39:57 -0300

    BFF ruta lists 6

--- src/app/api/lists/route.ts
+++ src/app/api/lists/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server"
 export async function GET(req: Request) {
   const res = await bffApi.get('/lists', {
     headers: {
-      cookie: (await cookies()).toString()
+      cookie: cookies().toString()
     }
   })
 
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
 
   const res = await  bffApi.post('/lists', body, {
     headers: {
-      cookie: (await cookies()).toString()
+      cookie: cookies().toString()
     }
   })  
 
