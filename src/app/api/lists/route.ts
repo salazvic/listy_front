d5f79a7449b2af33215bf30b89063679dddb84d0
salazvic <sadelto3011@gmail.com>
2026-01-21 12:31:59 -0300

    BFF ruta lists 10

--- src/app/api/lists/route.ts
+++ src/app/api/lists/route.ts
@@ -1,4 +1,4 @@
-import { bffApi } from "@/lib/api.bff"
+/* import { bffApi } from "@/lib/api.bff"
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
@@ -15,4 +15,12 @@ export async function POST(req: Request) {
   const res = await  bffApi.post('/lists', body)  
 
   return NextResponse.json(res.data, {status: 201})
+} */
+
+import { NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET() {
+  return NextResponse.json({ ok: true })
 }
