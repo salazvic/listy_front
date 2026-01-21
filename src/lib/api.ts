83d2df04fa93a1216befc967a1fc37ea8a3e47b7
salazvic <sadelto3011@gmail.com>
2026-01-21 12:07:19 -0300

    BFF ruta lists 7

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,59 +1,65 @@
-import axios from 'axios'
+import axios, { AxiosError, AxiosRequestConfig } from 'axios'
 import { useAuthStore } from '@/stores/auth.store'
-import { connectSockets } from '@/lib/socket'
+import { connectSockets, disconnectSockets } from '@/lib/socket'
+
+interface RetryConfig extends AxiosRequestConfig {
+  _retry?: boolean
+}
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: '/api', // BFF
   withCredentials: true,
 })
 
-/* let isRefreshing = false
-let queue: (() => void)[] = []
+let isRefreshing = false
+let queue: {
+  resolve: (value?: any) => void
+  reject: (reason?: any) => void
+}[] = []
+
+const processQueue = (error?: any) => {
+  queue.forEach(p => (error ? p.reject(error) : p.resolve()))
+  queue = []
+}
 
 api.interceptors.response.use(
   res => res,
-  async error => {
-    const auth = useAuthStore.getState() 
-    const originalRequest = error.config as any
-
-    const isAuthRoute = 
-      originalRequest.url?.includes('/auth/login') ||
-      originalRequest.url?.includes('/auth/refresh')
+  async (error: AxiosError) => {
+    const originalRequest = error.config as RetryConfig
 
     if (
-      error.response?.status === 401 &&
-      !originalRequest._retry &&
-      !isAuthRoute
+      error.response?.status !== 401 ||
+      originalRequest._retry ||
+      originalRequest.url?.includes('/auth/login') ||
+      originalRequest.url?.includes('/auth/refresh')
     ) {
-      originalRequest._retry = true
-
-      if(isRefreshing) {
-        return new Promise(resolve => {
-          queue.push(() => resolve(api(originalRequest)))
-        })
-      }
-
-      isRefreshing = true
-
-      try {
-        await api.post('/auth/refresh')
-        
-        queue.forEach(cb => cb())
-        queue = []
-
-        connectSockets()
-
-        return api(originalRequest)
-      } catch {
-        auth.logout()
-        return Promise.reject(error)
-      } finally {
-        isRefreshing = false
-      }
+      return Promise.reject(error)
     }
 
-    return Promise.reject(error)
+    originalRequest._retry = true
+
+    if (isRefreshing) {
+      return new Promise((resolve, reject) => {
+        queue.push({ resolve, reject })
+      }).then(() => api(originalRequest))
+    }
+
+    isRefreshing = true
+
+    try {
+      await api.post('/auth/refresh')
+      processQueue()
+      connectSockets()
+      return api(originalRequest)
+    } catch (err) {
+      processQueue(err)
+      useAuthStore.getState().logout()
+      disconnectSockets()
+      return Promise.reject(err)
+    } finally {
+      isRefreshing = false
+    }
   }
-) */
+)
 
 export default api
