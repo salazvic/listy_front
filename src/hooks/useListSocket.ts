f848d1d3577ab81dce1c82849caae3df30fde03e
salazvic <sadelto3011@gmail.com>
2026-02-07 14:43:29 -0300

    BFF ruta lists 27

--- src/hooks/useListSocket.ts
+++ src/hooks/useListSocket.ts
@@ -3,26 +3,51 @@ import { useListStore } from "@/stores/lists.store"
 import { Events } from "@/types/events.types"
 import { useEffect } from "react"
 
-export function useListSocket(idList: string) {
-  const updateList = useListStore(s => s.updateList)
-  const addUserList = useListStore(s => s.addUserList)
+export function useListSocket(listId: string) {
+  const {
+    upsertList,
+    deleteList,
+    addItemList,
+    updateItemList,
+    deleteItemList,
+    addUserList,
+    deleteUserList,
+    changeRoleUser,
+  } = useListStore()
 
   useEffect(() => {
-    const socketList = listSocket() 
+    const socket = listSocket()
+    if (!socket) return
 
-    if(!socketList) return
+    socket.emit(Events.LIST_JOIN, { listId })
 
-    socketList.emit(Events.LIST_JOIN, { listId: idList }) 
+    socket.on(Events.LIST_UPDATED, upsertList)
+    socket.on(Events.LIST_DELETED, ({ listId }) => deleteList(listId))
 
-    socketList.on(Events.LIST_UPDATED, (payload) => {
-      updateList(payload.id, payload.name)
-    })
+    socket.on(Events.ITEM_LIST_ADDED, ({ listId, item }) =>
+      addItemList(listId, item)
+    )
 
-    
+    socket.on(Events.ITEM_UPDATED, ({ listId, item }) =>
+      updateItemList(listId, item)
+    )
+
+    socket.on(Events.ITEM_DELETED, ({ listId, itemId }) =>
+      deleteItemList(listId, itemId)
+    )
+
+    socket.on(Events.SHARED_ADDED, ({ listId, user }) =>
+      addUserList(listId, user)
+    )
+
+    socket.on(Events.SHARED_USER_ROL, ({ listId, sharedId, role }) =>
+      changeRoleUser(listId, sharedId, role)
+    )
 
     return () => {
-      socketList.emit(Events.LIST_LEFT, {listId: idList});
-      socketList.off(Events.LIST_UPDATED)
-    }     
-  },[idList])
+      socket.emit(Events.LIST_LEFT, { listId })
+      socket.removeAllListeners()
+    }
+  }, [listId])
 }
+
